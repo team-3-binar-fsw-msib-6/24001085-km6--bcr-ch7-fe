d38ebfd06170c43daff494157e392aeed158f1d9
nidaftrnie <fitrianienida@gmail.com>
2024-05-08 20:48:52 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
