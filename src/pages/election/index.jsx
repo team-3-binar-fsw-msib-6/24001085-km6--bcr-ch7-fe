ab4f5fdce3e74b4c4e84fc2a1aa7e0dc1c73203a
nidaftrnie <fitrianienida@gmail.com>
2024-05-10 19:41:36 +0700

    feature: auth

---
+++ src/pages/election/index.jsx
@@ -0,0 +1,5 @@
+const Election = () => {
+  return <h1>Election - changed</h1>;
+};
+
+export default Election;
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,5 +1,5 @@
 const Register = () => {
-  return <h1>Register</h1>;
+  return <h1>Register Page</h1>;
 };
 
 export default Register;
