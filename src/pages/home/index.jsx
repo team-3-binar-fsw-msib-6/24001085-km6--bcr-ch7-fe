6092a617554a69ffcf4d85c6da20c8346919387d
Achmad Maulana Alawi <alawitch.b@gmail.com>
2024-05-17 14:02:30 +0700

    create new branch

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,80 @@
-const Home = () => {
-  return <h1>Home</h1>;
-};
+import { io } from "socket.io-client"
+import { useMemo, useEffect, useState } from "react"
+import { BarChart } from "@mui/x-charts"
 
-export default Home;
+const Home = () => {
+  const [DT, setData] = useState([])
+  const socket = useMemo(() => io("http://localhost:4000"))
+
+  useEffect(() => {
+    socket.on("update", (votes) => {
+      const voteCounts = {
+        1: 0,
+        2: 0,
+        3: 0,
+      }
+
+      votes.forEach((user) => {
+        voteCounts[user.vote]++
+      })
+
+      setData([voteCounts[1], voteCounts[2], voteCounts[3]])
+    })
+  }, [])
+
+  const updateVote = (payload) => {
+    socket.emit("vote", {
+      email: payload.email,
+      voteNumber: payload.voteNumber,
+    })
+  }
+  return (
+    <>
+      <h4>Voting</h4>
+      <div className="bar">
+        <BarChart
+          width={800}
+          height={350}
+          series={[
+            {
+              data: DT.length > 0 ? DT : [0, 0, 0],
+              id: "uvId",
+              label: "Votes",
+            },
+          ]}
+          xAxis={[
+            {
+              data: ["Capres 01", "Capres 02", "Capres 03"],
+              scaleType: "band",
+            },
+          ]}
+        />
+      </div>
+      <h3>
+        <u>Cast Vote</u>
+      </h3>
+      <div className="btn">
+        <button
+          className="myButton"
+          onClick={() => updateVote({ email: "testfromsocket", voteNumber: 1 })}
+        >
+          Capres 01
+        </button>
+        <button
+          className="myButton"
+          onClick={() => updateVote({ email: "testfromsocket", voteNumber: 2 })}
+        >
+          Capres 02
+        </button>
+        <button
+          className="myButton"
+          onClick={() => updateVote({ email: "testfromsocket", voteNumber: 3 })}
+        >
+          Capres 03
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Home
